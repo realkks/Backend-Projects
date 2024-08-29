b981ff39e1dd33712719582dff827da5deced1f2
= <=>
2024-08-29 22:33:25 +0530

    Moved db connection details to env.properties file and updated application properties file

--- Product-Service/UserService/src/main/java/com/productservice/userservice/Services/AuthService.java
+++ Product-Service/UserService/src/main/java/com/productservice/userservice/Services/AuthService.java
@@ -5,11 +5,12 @@ import com.productservice.userservice.Exceptions.UserNotFoundException;
 import com.productservice.userservice.Exceptions.WrongPasswordException;
 import com.productservice.userservice.Models.User;
 import com.productservice.userservice.Repository.UserRepo;
+import io.jsonwebtoken.Jwts;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.swing.text.html.Option;
-import java.util.Optional;
+import java.util.*;
 
 @Service
 public class AuthService {
@@ -47,6 +48,21 @@ public class AuthService {
             }else{
                 throw new WrongPasswordException("Incorrect password");
             }
+        }
+
+        private String createJwtToken(Long user_id, List<String> roles, String email){
+            Map<String, Object> dataInJwt = new HashMap<>();
+            dataInJwt.put("user_id",user_id);
+            dataInJwt.put("roles",roles);
+            dataInJwt.put("email",email);
+
+            Jwts.builder()
+                    .claims(dataInJwt)     // Adding data to jwt token
+                    .issuedAt(new Date(System.currentTimeMillis()))  // Setting issue date
+                    // System.currentTimeMillis() will return currenttime in milliseconds
+                    // 1000L * 60 * 60 * 24 - It represnts 1 day in milliseconds
+                    .expiration(new Date(System.currentTimeMillis()+ 1000L * 60 * 60 * 24 * 30)); // Setting expiration date
+            return null;
         }
 
 }
